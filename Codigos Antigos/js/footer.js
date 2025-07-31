7467a131cc627197eef74fa0f50126fe830b1116
Iago Borges Santana <141869310+Sonwcracker@users.noreply.github.com>
2025-07-31 00:31:47 -0300

    projeto atualizado

--- Codigos Antigos/js/footer.js
+++
@@ -1,5 +0,0 @@
-// js/footer.js
-window.addEventListener('DOMContentLoaded', async () => {
-  const footer = await fetch("footer.html").then(res => res.text());
-  document.getElementById("footer-container").innerHTML = footer;
-});
